a8ff9633d88cf534b25ac44b8261c5b5cf6375e2
NEXTGroup <jiwlee0054@nextgroup.or.kr>
2023-04-03 17:53:47 +0900

    230403.부하 패턴 및 발전기 패턴(불확실성 반영) 추가

--- lib/parameters.py
+++ lib/parameters.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -10,8 +11,8 @@ class ProbOptions(Customize):
         # MW
         self.power_demand_max = 100
 
-        self.loc_damand_factor = "data/1. 2021_표준산업분류(중분류),주택용 1_24시 상대계수.xlsx"
-        self.loc_pv_factor_IC = "data/전남_한국서부_영암F1_2020_태양광.xlsx"
+        self.loc_damand_factor = "C:/Users/NEXTGroup/Dropbox/모델 개발/OptimalPortfolio/data/부하/1. 2021_표준산업분류(중분류),주택용 1_24시 상대계수.xlsx"
+        self.loc_pv_factor_IC = "C:/Users/NEXTGroup/Dropbox/모델 개발/OptimalPortfolio/data/발전기/전남_한국서부_영암F1_2020_태양광.xlsx"
 
 
         self.choice_number_of_demand_type = 'customize'  # customize / all
@@ -29,9 +30,47 @@ class ProbOptions(Customize):
             self.demand_type_list = ['1차 금속 제조업', '가구 제조업', '고무제품 및 플라스틱제품 제조업']
 
 
-class LoadInputFile:
+class CallInput:
     def __init__(self, options: ProbOptions):
-        self.factor_demand_all_raw = pd.read_excel(options.loc_damand_factor)
+        month = 1
+        self.month_to_row = dict()
+        row = 0
+        for d in [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]:
+            self.month_to_row[month] = np.arange(row, row + d, 1)
+            row += d
+            month += 1
+
 
         if options.choice_region_gen == 'JN':
             self.factor_gen_raw = pd.read_excel(options.loc_pv_factor_IC, sheet_name=None)
+
+        self.call_dmd(options)
+        self.call_gen(options)
+
+    def call_dmd(self, options):
+        raw = pd.read_excel(options.loc_damand_factor)
+        self.factor_dmd_list = dict()
+
+        for k in options.demand_type_list:
+            demand_arr = np.zeros((365, 24))
+            if raw[k].shape[0] == 288:
+                for m in range(1, 13, 1):
+                    demand_arr[self.month_to_row[m], :] = raw[k].loc[(m - 1) * 24: m * 24 - 1].values
+
+            self.factor_dmd_list[k] = demand_arr / np.max(demand_arr)
+
+    def call_gen(self, options):
+        gen = np.zeros((365, 24))
+
+        if options.choice_region_gen == 'JN':
+            raw = pd.read_excel(options.loc_pv_factor_IC, sheet_name=None)
+            raw1 = raw['Sheet1'].loc[:, 1:24] / raw['Sheet2'].iloc[0].iloc[0]
+            raw1.columns = np.arange(0, 24, 1)
+
+            for d in range(0, 365, 1):
+                for h in range(0, 24, 1):
+                    mean_ = raw1.loc[d, h]
+                    std_ = np.std(raw1.loc[:, h].values)
+                    gen[d, h] = abs(np.random.normal(loc=mean_, scale=std_))
+
+        self.factor_gen = gen
